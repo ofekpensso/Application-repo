2367367c04f28f74d31d89bf865f74946d98523f
ofek penso <ofekpenso@gmail.com>
2026-01-11 12:12:29 +0000

    app

--- api.py
+++ api.py
@@ -1,7 +1,16 @@
 from flask import Flask, jsonify
+
 app = Flask(__name__)
+
+# --- החלק החדש שהוספנו ---
+@app.route('/')
+def home():
+    return "I finish the deploy stage", 200
+# -------------------------
+
 @app.get("/health")
 def health():
     return jsonify({"status": "ok"}), 200
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
